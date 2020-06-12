6502a45a2d5403691923a4d94082eee28036215e
root <foxmart36@gmail.com>
2020-06-12 19:03:49 +0400

    Saw off presentation && audio-test

--- bigbluebutton-html5/imports/ui/components/media/container.jsx
+++ bigbluebutton-html5/imports/ui/components/media/container.jsx
@@ -12,7 +12,6 @@ import Media from './component';
 import MediaService, { getSwapLayout, shouldEnableSwapLayout } from './service';
 import PresentationPodsContainer from '../presentation-pod/container';
 import ScreenshareContainer from '../screenshare/container';
-import DefaultContent from '../presentation/default-content/component';
 import ExternalVideoContainer from '../external-video-player/container';
 import Storage from '../../services/storage/session';
 import { withDraggableConsumer } from './webcam-draggable-overlay/context';
@@ -109,7 +108,7 @@ export default withDraggableConsumer(withModalMounter(withTracker(() => {
   const hidePresentation = getFromUserSettings('bbb_hide_presentation', LAYOUT_CONFIG.hidePresentation);
   const { current_presentation: hasPresentation } = MediaService.getPresentationInfo();
   const data = {
-    children: <DefaultContent />,
+    children: false,
     audioModalIsOpen: Session.get('audioModalIsOpen'),
   };
 
